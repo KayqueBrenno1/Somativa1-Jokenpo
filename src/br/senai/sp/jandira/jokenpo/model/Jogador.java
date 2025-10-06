6384cc1f3ca25090bfddbad1ea298a4267e8d490
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2025-10-06 14:12:35 -0300

    Função da jogada do computador adicionada

--- src/br/senai/sp/jandira/jokenpo/model/Jogador.java
+++ src/br/senai/sp/jandira/jokenpo/model/Jogador.java
@@ -1,16 +1,23 @@
 package br.senai.sp.jandira.jokenpo.model;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Jogador {
     int numeroDaJogada;
+    int numeroDaJogadaDoComputador;
     String jogada;
+    String jogadaComputador;
+    String resultado;
+    String decisao;
+    Scanner leitor = new Scanner(System.in);
+    Random random = new Random();
 
     public void escolherJogada(){
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("-------------------------------");
-        System.out.println("*** Vamos jogar ***");
+        System.out.println("********* VAMOS JOGAR *********");
         System.out.println("-------------------------------");
         System.out.println("Escolha uma opção:");
         System.out.println("(1) Pedra");
@@ -33,5 +40,21 @@ public class Jogador {
             jogada = "Jogada não disponível";
             escolherJogada();
         }
+
+        receberJogadaDoComputador();
     }
-}
+    public void receberJogadaDoComputador(){
+        numeroDaJogadaDoComputador = random.nextInt(3) + 1;
+        verificarJogadaDoComputador();
+    }
+    public void verificarJogadaDoComputador(){
+        if (numeroDaJogadaDoComputador == 1){
+            jogadaComputador = "Pedra";
+        }else if (numeroDaJogadaDoComputador == 2){
+            jogadaComputador = "Papel";
+        }else {
+            jogadaComputador = "Tesoura";
+        }
+
+
+    }
